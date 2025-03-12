4721fba88e56950c0793bbcb7a3c7f1d5848534c
kajol_1609 <kajol_murmu@epam.com>
2025-03-12 16:49:13 +0530

    task04

--- task04/app/lambdas/sns_handler/index.js
+++ task04/app/lambdas/sns_handler/index.js
@@ -1,8 +1,12 @@
-exports.handler = async (event) => {
-    // TODO implement
-    const response = {
+exports.snsHandler = async (event) => {
+    console.log("SNS Event Received:", JSON.stringify(event, null, 2));
+
+    event.Records.forEach(record => {
+        console.log("SNS Message:", record.Sns.Message);
+    });
+
+    return {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
+        body: JSON.stringify('SNS Message Processed'),
     };
-    return response;
 };
