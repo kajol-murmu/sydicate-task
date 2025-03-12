10df2de7757fe0d29abe8222221412c160cc9038
kajol_1609 <kajol_murmu@epam.com>
2025-03-12 17:44:35 +0530

    wtf

--- task04/app/lambdas/sqs_handler/index.js
+++ task04/app/lambdas/sqs_handler/index.js
@@ -1,8 +1,12 @@
-exports.handler = async (event) => {
-    // TODO implement
-    const response = {
+exports.sqsHandler = async (event) => {
+    console.log("SQS Event Received:", JSON.stringify(event, null, 2));
+
+    event.Records.forEach(record => {
+        console.log("SQS Message Body:", record.body);
+    });
+
+    return {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
+        body: JSON.stringify('SQS Message Processed'),
     };
-    return response;
 };
